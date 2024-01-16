a6aa42044d15f143c8fef65546bb940abb73addc
wjgra <25494659+wjgra@users.noreply.github.com>
2024-01-16 20:03:19 +0000

    Implement nested DCT

--- jpeg/inc/discrete_cosine_transform.hpp
+++ jpeg/inc/discrete_cosine_transform.hpp
@@ -2,6 +2,7 @@
 #define _JPEG_DISCRETE_COSINE_TRANSFORM_HPP_
 
 #include <array>
+#include <span>
 #include <cmath>
 #include <numbers>
 
@@ -23,18 +24,22 @@ namespace jpeg{
         virtual ColourMappedBlock::ChannelBlock applyInverseTransform(DCTChannelOutput  const& inputChannel) const = 0;
     };
 
-    /* Very slowly calculates the DCT/IDCT by direct calculation (in the naive manner) */
+    /* Calculates the 2D DCT/IDCT by direct calculation. */
     class NaiveCosineTransformer : public DiscreteCosineTransformer{
     protected:
         DCTChannelOutput applyTransform(ColourMappedBlock::ChannelBlock const& inputChannel) const override;
         ColourMappedBlock::ChannelBlock applyInverseTransform(DCTChannelOutput  const& inputChannel) const override;
     };
 
-    class FastCosineTransformer : public DiscreteCosineTransformer{
+    /* Calculates the 2D DCT by applying the 1D transform recursively. 
+       While this is quicker than the naive transformer in un-optimised builds, the naive transformer is more effectively
+       optimised in -O3 builds (naive is ~3x faster!). */
+    class NestedCosineTransformer : public DiscreteCosineTransformer{
     protected:
         DCTChannelOutput applyTransform(ColourMappedBlock::ChannelBlock const& inputChannel) const override;
         ColourMappedBlock::ChannelBlock applyInverseTransform(DCTChannelOutput  const& inputChannel) const override;
+    private:
+        std::array<float, BlockGrid::blockSize> apply1DTransform(std::array<int8_t, BlockGrid::blockSize> const& input) const;
     };
-
 }
 #endif
--- jpeg/src/discrete_cosine_transform.cpp
+++ jpeg/src/discrete_cosine_transform.cpp
@@ -16,7 +16,7 @@ jpeg::DCTChannelOutput jpeg::NaiveCosineTransformer::applyTransform(ColourMapped
     DCTChannelOutput output;
     for (size_t u = 0 ; u < BlockGrid::blockSize ; ++u){
         for (size_t v = 0 ; v < BlockGrid::blockSize ; ++v){
-            float scaleFactor = 0.25f * ((u == 0) ? 1/std::sqrt(2.0f) : 1) * ((v == 0) ? 1/std::sqrt(2.0f) : 1);
+            float const scaleFactor = 0.25f * ((u == 0) ? 1/std::sqrt(2.0f) : 1) * ((v == 0) ? 1/std::sqrt(2.0f) : 1);
             float accumulator = 0;
             for (size_t x = 0 ; x < BlockGrid::blockSize ; ++x){
                 for (size_t y = 0 ; y < BlockGrid::blockSize ; ++y){
@@ -38,7 +38,7 @@ jpeg::ColourMappedBlock::ChannelBlock jpeg::NaiveCosineTransformer::applyInverse
             float accumulator = 0;
             for (size_t u = 0 ; u < BlockGrid::blockSize ; ++u){
                 for (size_t v = 0 ; v < BlockGrid::blockSize ; ++v){
-                    float scaleFactor = 0.25f * ((u == 0) ? 1/std::sqrt(2.0f) : 1) * ((v == 0) ? 1/std::sqrt(2.0f) : 1);
+                    float const scaleFactor = 0.25f * ((u == 0) ? 1/std::sqrt(2.0f) : 1) * ((v == 0) ? 1/std::sqrt(2.0f) : 1);
                     accumulator += scaleFactor * inputChannel.data[u + v * BlockGrid::blockSize] 
                         * std::cos((2.0f * x + 1) * u * std::numbers::pi_v<float> / 16.0f)
                         * std::cos((2.0f * y + 1) * v * std::numbers::pi_v<float> / 16.0f);
@@ -64,67 +64,63 @@ jpeg::ColourMappedBlock::ChannelBlock jpeg::NaiveCosineTransformer::applyInverse
     return channelData;
 }
 
-jpeg::DCTChannelOutput jpeg::FastCosineTransformer::applyTransform(ColourMappedBlock::ChannelBlock const& inputChannel) const{
+jpeg::DCTChannelOutput jpeg::NestedCosineTransformer::applyTransform(ColourMappedBlock::ChannelBlock const& inputChannel) const{
     std::array<int8_t, BlockGrid::blockElements> offsetChannelData;
     for (size_t i = 0 ; i < BlockGrid::blockElements ; ++i){
         offsetChannelData[i] = inputChannel[i] - 128;
     }
     DCTChannelOutput output;
 
-    
+    for (size_t v = 0 ; v < BlockGrid::blockSize ; ++v){
+        float const outerScaleFactor = 0.5f * ((v == 0) ? 1/std::sqrt(2.0f) : 1);
+        std::array<float, BlockGrid::blockSize> outerAccumulator{};
+        for (size_t y = 0 ; y < BlockGrid::blockSize ; ++y){
 
-    ///////////////////////////////
-    
-    for (size_t u = 0 ; u < BlockGrid::blockSize ; ++u){
-        for (size_t v = 0 ; v < BlockGrid::blockSize ; ++v){
-            float scaleFactor = 0.25f * ((u == 0) ? 1/std::sqrt(2.0f) : 1) * ((v == 0) ? 1/std::sqrt(2.0f) : 1);
-            float accumulator = 0;
-            for (size_t x = 0 ; x < BlockGrid::blockSize ; ++x){
-                for (size_t y = 0 ; y < BlockGrid::blockSize ; ++y){
-                    accumulator += offsetChannelData[x + y * BlockGrid::blockSize] 
-                        * std::cos((2.0f * x + 1) * u * std::numbers::pi_v<float> / 16.0f)
-                        * std::cos((2.0f * y + 1) * v * std::numbers::pi_v<float> / 16.0f);
-                }
+            /* std::array<float, BlockGrid::blockSize> innerDCT{};
+            for (size_t u = 0 ; u < BlockGrid::blockSize ; ++u){
+                float const innerScaleFactor = 0.5f * ((u == 0) ? 1/std::sqrt(2.0f) : 1);
+                float accumulator = 0;
+                
+                    for (size_t x = 0 ; x < BlockGrid::blockSize ; ++x){
+                        accumulator += offsetChannelData[x + y * BlockGrid::blockSize] 
+                                    * std::cos((2.0f * x + 1) * u * std::numbers::pi_v<float> / 16.0f);
+                    }
+                innerDCT[u] = innerScaleFactor * accumulator;
+            } */
+
+            std::array<int8_t, BlockGrid::blockSize> rowInput{};
+            std::copy(offsetChannelData.begin() + y * BlockGrid::blockSize, offsetChannelData.begin() + (y + 1) * BlockGrid::blockSize, rowInput.begin());
+            auto innerDCT = apply1DTransform(rowInput);
+
+            // vector operation
+            for (int i = 0 ; i < 8 ; ++i){
+                outerAccumulator[i] += innerDCT[i] * std::cos((2.0f * y + 1) * v * std::numbers::pi_v<float> / 16.0f);
             }
-            output.data[u + v * BlockGrid::blockSize] = scaleFactor * accumulator;
+
         }
+        // vector operation
+        for (int i = 0 ; i < 8 ; ++i){
+            output.data[i + v * BlockGrid::blockSize] = outerScaleFactor * outerAccumulator[i];
+        }
+
     }
     return output;
 }
 
-jpeg::ColourMappedBlock::ChannelBlock jpeg::FastCosineTransformer::applyInverseTransform(DCTChannelOutput  const& inputChannel) const{
-
-    std::array<int8_t, BlockGrid::blockElements> offsetChannelData;
-    for (size_t x = 0 ; x < BlockGrid::blockSize ; ++x){
-        for (size_t y = 0 ; y < BlockGrid::blockSize ; ++y){
-            float accumulator = 0;
-            for (size_t u = 0 ; u < BlockGrid::blockSize ; ++u){
-                for (size_t v = 0 ; v < BlockGrid::blockSize ; ++v){
-                    float scaleFactor = 0.25f * ((u == 0) ? 1/std::sqrt(2.0f) : 1) * ((v == 0) ? 1/std::sqrt(2.0f) : 1);
-                    accumulator += scaleFactor * inputChannel.data[u + v * BlockGrid::blockSize] 
-                        * std::cos((2.0f * x + 1) * u * std::numbers::pi_v<float> / 16.0f)
-                        * std::cos((2.0f * y + 1) * v * std::numbers::pi_v<float> / 16.0f);
-                }
-            }
-            int8_t const maxInt{127};
-            int8_t const minInt{-128};
-            if (accumulator < minInt){
-                offsetChannelData[x + y * BlockGrid::blockSize] = minInt;
-            }
-            else if (accumulator > maxInt){
-                offsetChannelData[x + y * BlockGrid::blockSize] = maxInt;
-            }
-            else{
-                offsetChannelData[x + y * BlockGrid::blockSize] = accumulator;
-            }
+std::array<float, jpeg::BlockGrid::blockSize> jpeg::NestedCosineTransformer::apply1DTransform(std::array<int8_t, BlockGrid::blockSize> const& input) const{
+    std::array<float, jpeg::BlockGrid::blockSize> output;
+    for (size_t u = 0 ; u < BlockGrid::blockSize ; ++u){
+        float const scaleFactor = 0.5f * ((u == 0) ? 1/std::sqrt(2.0f) : 1);
+        float accumulator = 0;
+        for (size_t x = 0 ; x < BlockGrid::blockSize ; ++x){
+            accumulator += input[x] * std::cos((2.0f * x + 1) * u * std::numbers::pi_v<float> / 16.0f);
         }
+        output[u] = scaleFactor * accumulator;
     }
+    return output;
+}
 
-    ///////////////////////////
-
-    ColourMappedBlock::ChannelBlock channelData;
-    for (size_t i = 0 ; i < BlockGrid::blockElements ; ++i){
-        channelData[i] = offsetChannelData[i] + 128;
-    }
-    return channelData;
+jpeg::ColourMappedBlock::ChannelBlock jpeg::NestedCosineTransformer::applyInverseTransform(DCTChannelOutput  const& inputChannel) const{
+    /* Unimplemented */
+    return ColourMappedBlock::ChannelBlock();
 }
