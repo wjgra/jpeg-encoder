92ea5dbc6b57913ae3c47994aca828201e1957b6
wjgra <25494659+wjgra@users.noreply.github.com>
2024-01-17 01:19:41 +0000

    Implement web interface

--- examples/web-app.cpp
+++ examples/web-app.cpp
@@ -1,6 +1,4 @@
-#ifdef __EMSCRIPTEN__
 #include <emscripten.h>
-#endif
 
 #define SDL_MAIN_HANDLED
 #include <SDL_main.h>
@@ -36,47 +34,18 @@ bool mainLoop(Window& window){
     return true;
 }
 
-#ifdef __EMSCRIPTEN__
 void mainLoopCallback(void* window){
     mainLoop(*static_cast<Window*>(window));
 }
-#endif
 
-int main(int argc, char *argv[]){
-    std::vector<std::string> arguments(argv + 1, argv + argc);
-    int qualityValue = 80;
-    int scale = 1;
-    std::string inputBmpImagePath = "img/leclerc.bmp";
-    for(auto arg = arguments.begin() ; arg != arguments.end() ; ++arg){
-        if (strcmp(arg->c_str(), "-q") == 0){
-            if (arg != arguments.end() - 1){
-                ++arg;
-                qualityValue = std::stoi(std::string(*arg));
-            }
-        }
-        else if (strcmp(arg->c_str(), "-i") == 0){
-            if (arg != arguments.end() - 1){
-                ++arg;
-                inputBmpImagePath = *arg;
-            }
-        }
-        else if (strcmp(arg->c_str(), "-s") == 0){
-            if (arg != arguments.end() - 1){
-                ++arg;
-                scale = std::stoi(std::string(*arg));
-            }
-        }
-    }
-
-    // Load test image
-    
-    jpeg::BitmapImageRGB inputBmp(inputBmpImagePath);
+int encodeDecodeImage(std::string const& path = "img-cc/matterhorn.bmp", int quality = 80, bool save = false){
+    jpeg::BitmapImageRGB inputBmp(path);
     
     if (inputBmp.width == 0 || inputBmp.height == 0){
         return EXIT_FAILURE;
     }
 
-    Window window(2 * inputBmp.width * scale, inputBmp.height * scale, "BMP-to-JPEG");
+    Window window(2 * inputBmp.width, inputBmp.height, "BMP-to-JPEG");
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Renderer* renderer = SDL_CreateRenderer(window.getWindow(), -1, 0);
     if (!window.getWindow() || !renderer) return EXIT_FAILURE;
@@ -85,13 +54,13 @@ int main(int argc, char *argv[]){
     SDL_Texture* inputBmpTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STATIC, inputBmp.width, inputBmp.height);
     SDL_UpdateTexture(inputBmpTexture, nullptr, inputBmp.data.data(), inputBmp.width * 3);
     SDL_RenderClear(renderer);
-    SDL_Rect leftHalf = {.x = 0, .y = 0,  .w = inputBmp.width * scale, .h = inputBmp.height * scale};
+    SDL_Rect leftHalf = {.x = 0, .y = 0,  .w = inputBmp.width, .h = inputBmp.height};
     SDL_RenderCopy(renderer, inputBmpTexture, nullptr, &leftHalf);
     SDL_RenderPresent(renderer);
 
     // Encode image as JPEG
     auto tStart = std::chrono::high_resolution_clock::now();
-    jpeg::JPEGEncoder enc(inputBmp, qualityValue);
+    jpeg::JPEGEncoder enc(inputBmp, quality);
     auto tEnd = std::chrono::high_resolution_clock::now();
 
     auto timeToEncode = 1e-3 * std::chrono::duration_cast<std::chrono::microseconds>(tEnd - tStart).count();
@@ -104,7 +73,7 @@ int main(int argc, char *argv[]){
     // Decode encoded JPEG image
     jpeg::JPEGImage outputJpeg = enc.getJPEGImageData();
     tStart = std::chrono::high_resolution_clock::now();
-    jpeg::JPEGDecoder dec(outputJpeg, qualityValue);
+    jpeg::JPEGDecoder dec(outputJpeg, quality);
     tEnd = std::chrono::high_resolution_clock::now();
 
     auto timeToDecode = 1e-3 * std::chrono::duration_cast<std::chrono::microseconds>(tEnd - tStart).count();
@@ -121,16 +90,35 @@ int main(int argc, char *argv[]){
     // Display post-encoding image in right half of window
     SDL_Texture* outputBmpTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STATIC, outputBmp.width, outputBmp.height);
     SDL_UpdateTexture(outputBmpTexture, nullptr, outputBmp.data.data(), outputBmp.width * 3);
-    SDL_Rect rightHalf = {.x = inputBmp.width * scale, .y = 0,  .w = outputBmp.width * scale, .h = outputBmp.height * scale};
+    SDL_Rect rightHalf = {.x = inputBmp.width , .y = 0,  .w = outputBmp.width, .h = outputBmp.height};
     SDL_RenderCopy(renderer, outputBmpTexture, nullptr, &rightHalf);
     SDL_RenderPresent(renderer);
 
-    #ifndef __EMSCRIPTEN__
-    while(mainLoop(window)){}
-    #else
-    emscripten_set_main_loop_arg(&mainLoopCallback, &window, 0, 1);
-    #endif
-    
+    emscripten_set_main_loop_arg(&mainLoopCallback, &window, 0, true);
+
     SDL_Quit();
     return EXIT_SUCCESS;
+}
+
+extern "C" {
+    EMSCRIPTEN_KEEPALIVE void encodeDecodeImageLeclerc(int quality){
+        emscripten_cancel_main_loop();
+        encodeDecodeImage("img-cc/leclerc-ferrari.bmp", quality);
+    }
+
+    EMSCRIPTEN_KEEPALIVE void encodeDecodeImageMatterhorn(int quality){
+        emscripten_cancel_main_loop();
+        encodeDecodeImage("img-cc/matterhorn.bmp", quality);
+    }
+
+    EMSCRIPTEN_KEEPALIVE void encodeDecodeImageSaturn(int quality){
+        emscripten_cancel_main_loop();
+        encodeDecodeImage("img-cc/cassini-saturn.bmp", quality);
+    }
+
+    // and uploaded...
+}
+
+EMSCRIPTEN_KEEPALIVE int main(){
+    return encodeDecodeImage();
 }
