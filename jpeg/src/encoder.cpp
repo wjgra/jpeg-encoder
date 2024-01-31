98637463ac69f6484a6005406f801eae98a394ba
wjgra <25494659+wjgra@users.noreply.github.com>
2024-01-31 21:24:38 +0000

    Implement SOS marker encoding/decoding

--- jpeg/src/encoder.cpp
+++ jpeg/src/encoder.cpp
@@ -9,27 +9,32 @@ jpeg::Encoder::Encoder(BitmapImageRGB const& inputImage,
                        EntropyEncoder const& entropyEncoder)
                        /* Issue: implement sequential/progressive option; downsampling option */
 {
-    outputImage.compressedImageData.clearStream();
-    encodeHeader(inputImage, outputImage.compressedImageData);
-    size_t startOfScanData = outputImage.compressedImageData.getSize();
-    InputBlockGrid blockGrid(inputImage);
-    std::array<int16_t, 3> lastDCValues = {0,0,0};
-    for (auto const& block : blockGrid){
-        ColourMappedBlock colourMappedBlock = colourMapper.map(block);
-        for (size_t channel = 0 ; channel < 3 ; ++channel){
-            DCTChannelOutput dctData = discreteCosineTransformer.transform(colourMappedBlock.data[channel]);
-            QuantisedChannelOutput quantisedOutput = quantiser.quantise(dctData, colourMapper.isLuminanceComponent(channel));
-            entropyEncoder.encode(quantisedOutput, lastDCValues[channel], outputImage.compressedImageData, colourMapper.isLuminanceComponent(channel));
+    try{
+        outputImage.compressedImageData.clearStream();
+        encodeHeader(inputImage, outputImage.compressedImageData);
+        size_t startOfScanData = outputImage.compressedImageData.getSize();
+        InputBlockGrid blockGrid(inputImage);
+        std::array<int16_t, 3> lastDCValues = {0,0,0};
+        for (auto const& block : blockGrid){
+            ColourMappedBlock colourMappedBlock = colourMapper.map(block);
+            for (size_t channel = 0 ; channel < 3 ; ++channel){
+                DCTChannelOutput dctData = discreteCosineTransformer.transform(colourMappedBlock.data[channel]);
+                QuantisedChannelOutput quantisedOutput = quantiser.quantise(dctData, colourMapper.isLuminanceComponent(channel));
+                entropyEncoder.encode(quantisedOutput, lastDCValues[channel], outputImage.compressedImageData, colourMapper.isLuminanceComponent(channel));
+            }
         }
-    }
-    stuffAlignedBytes(outputImage.compressedImageData, startOfScanData);
-    // Push end of image marker
-    outputImage.compressedImageData.pushIntoAlignment();
-    outputImage.compressedImageData.pushWord(markerEndOfImageSegmentEOI);
+        stuffAlignedBytes(outputImage.compressedImageData, startOfScanData);
+        // Push end of image marker
+        outputImage.compressedImageData.pushIntoAlignment();
+        outputImage.compressedImageData.pushWord(markerEndOfImageSegmentEOI);
 
-    outputImage.width = inputImage.width;// to remove
-    outputImage.height = inputImage.height; // to remove
-    outputImage.fileSize = outputImage.compressedImageData.getSize();
+        outputImage.width = inputImage.width;// to remove
+        outputImage.height = inputImage.height; // to remove
+        outputImage.fileSize = outputImage.compressedImageData.getSize();
+    }
+    catch(std::exception const& e){
+        std::cout << "[Error]: " << e.what() << "\n";
+    }
 }
 
 /* Issue: currently hardcoded with baseline parameters*/
@@ -77,7 +82,21 @@ void jpeg::Encoder::encodeHeader(BitmapImageRGB const& inputImage, BitStream& ou
     // DHT
 
     // SOS 
-
+    outputStream.pushWord(markerStartOfScanSegmentSOS);
+    outputStream.pushWord(12); // length
+    outputStream.pushByte(3); // Number of components
+    // First component
+    outputStream.pushByte(0); // ID
+    outputStream.pushByte(0); // Huffman table
+    // Second component
+    outputStream.pushByte(1); // ID
+    outputStream.pushByte(1); // Huffman table
+    // Third component
+    outputStream.pushByte(2); // ID
+    outputStream.pushByte(1); // Huffman table
+    // Skip bytes
+    outputStream.pushWord(0x0000);
+    outputStream.pushByte(0x00);
 }
 
 void jpeg::Encoder::stuffAlignedBytes(BitStream& outputStream, size_t startOfScanData) const{
